d3d5c366485e0fb7622bd6ce4368daab787aae81
cviazzo17 <carlaviazzo17@gmail.com>
2025-03-20 14:33:26 -0300

    Upload ipynb for tercile-based forecasts

---
+++ data_asfloat32.py
@@ -0,0 +1,26 @@
+"""Cast numpy.ndarray()s' type of float from"""
+
+import os
+import numpy as np
+
+path_in = "./data_float32"
+path_out = "./data"
+
+list_files = os.listdir(path_in)
+print(list_files)
+
+for file in list_files:
+    full_name = os.path.join(path_in, file)
+    data = np.load(full_name)
+
+    u10m = data["U10M"]
+    v10m = data["V10M"]
+    lon = data["longitude"]
+    lat = data["latitude"]
+
+    u10m = u10m.astype("float16")
+    v10m = v10m.astype("float16")
+    ws10m = u10m**2 + v10m**2
+
+    full_name_out = os.path.join(path_out, file)
+    np.savez(full_name_out, U10M=u10m, V10M=v10m, W10M=ws10m, latitude=lat, longitude=lon)
